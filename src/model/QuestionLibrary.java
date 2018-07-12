fc672b48c07d58a0d394f6d050ef47557c8662fc
Võ Văn Vũ <vanvu2912@gmail.com>
2018-07-12 23:04:55 +0700

    update question library

--- src/model/QuestionLibrary.java
+++ src/model/QuestionLibrary.java
@@ -16,12 +16,10 @@ public class QuestionLibrary {
 		Question question1 = new Question("Viet Nam co bao nhieu tinh thanh?", "62", "63", "64", "65", "b");
 		Question question2 = new Question("Thanh pho cua tinh Phu Yen", "Nha Trang", "Quy Nhon", "Tuy Hoa",
 				"Mu Cang Chai", "c");
-		Question question3 = new Question("JAV la viet tat cua tu gi?", "Japanese Adult Video", "Japan and Vietnam",
-				"Japan Anti Virus", "Tat ca deu sai", "d");
-		Question question4 = new Question("Chi pheo sinh ra o dau?", "Lo gach cu", "Lo gach moi", "Lo ton",
-				"Lo banh mi", "a");
-		Question question5 = new Question("Bien phap tranh thai nao an toan, it gay hai cho con nguoi?",
-				"Thuoc tranh thai khan cap", "That ong dan tinh", "Cat ong dan trung", "Bao cao su", "d");
+		Question question3 = new Question("2018 la nam con gi?", "Cho", "Ga", "Bo", "Khi", "a");
+		Question question4 = new Question("1+1=", "3", "4", "2", "Tat ca deu sai", "c");
+		Question question5 = new Question("Ten game ban dang choi la gi?", "Find The Dog", "Find The Fish",
+				"Find The Honey", "Honey The Find", "c");
 		questions.add(question1);
 		questions.add(question2);
 		questions.add(question3);
